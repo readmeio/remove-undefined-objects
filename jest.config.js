0c5af78962b2e9f64c3fc6ee7e31a6e133bcd87f
Jon Ursenbach <jon@ursenba.ch>
2023-01-03 09:55:01 -0800

    chore(deps-dev): bumping deps

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
   coveragePathIgnorePatterns: ['/dist', '/node_modules'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '__tests__/tsconfig.json',
-    },
-  },
   modulePaths: ['<rootDir>'],
-  preset: 'ts-jest/presets/js-with-ts',
   roots: ['<rootDir>'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$',
-  transform: {},
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '__tests__/tsconfig.json',
+      },
+    ],
+  },
 };
